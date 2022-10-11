1e0705e770c726f0c266580c38bc96a6bb1240e4
Sanchous <sasha2405200232409@gmail.com>
2022-10-11 22:57:08 +0300

    ad new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleInc = () => {
+    setCount(count + 1);
+  };
+  const handleDec = () => {
+    setCount(count - 1);
+  };
+
+  // const checkNum = () => {
+  //   if (count % 2 === 0) {
+  //     console.log("+");
+  //     return "`Четное`";
+  //   } else {
+  //     console.log("-");
+  //     return "`Нечетное`";
+  //   }
+  // };
+  // const displayCheck = () => {
+  //   checkNum()
+  // };
+
+  const divStyle = {
+    width: "1440px",
+    height: "600px",
+    fontSize: "30px",
+    paddingLeft: "550px",
+    color: "blue",
+    backgroundColor: "pink",
+  };
+  const parStyle = {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "20px",
+    paddingBottom: "20px",
+
+    width: "200px",
+    border: "2px solid white",
+    borderImage: "linear-gradient(-45deg, #3498db, #e67e22) 1",
+  };
+  const btnStyle = {
+    width: "130px",
+    height: " 45px",
+    background: " #ffffff",
+    border: "1px solid rgba(0, 0, 0, 0.6)",
+    borderRadius: "50px",
+    cursor: "pointer",
+    opacity: "1",
+    fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+    fontSize: "12px",
+    fontWeight: "500",
+    letterSpacing: "0.7px",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={divStyle}>
+      <p style={parStyle}> {count}</p>
+      <button style={btnStyle} onClick={handleInc}>
+        Добавить
+      </button>
+      <button style={btnStyle} onClick={handleDec}>
+        Отнять
+      </button>
+      {/* <button style={btnStyle} >
+        Проверка
+      </button> */}
+      <p>Вы ввели {count % 2 === 0 ?"четное":"нечетное" } число!</p>
     </div>
   );
 }
